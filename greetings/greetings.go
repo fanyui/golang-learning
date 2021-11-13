5bd16cb230d3cd36d432037cc3cf07eefefcc580
Harisu Fanyui <fanyuiharisu@gmail.com>
2021-11-13 02:50:43 +0100

    Feature: Added randome generating of the greeting text, this makes use of the go math, time function as well as go slice

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // hello returns a greeting for the named person
@@ -11,6 +13,19 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New(" Empty name")
 	}
-	message := fmt.Sprintf("Hi, %v . Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	// a slice of message format
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
