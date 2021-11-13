f7177ded2c1e70e8c156ba43532672c29f852c4a
Harisu Fanyui <fanyuiharisu@gmail.com>
2021-11-13 22:21:35 +0100

    Feature: Return greetings for multiple peoples, this makes use of the slice and map

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Harisu", "Gladys", "Samantha", "Darrin"}
+	message, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
